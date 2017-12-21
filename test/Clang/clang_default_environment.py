ad0b6375e9fa946a3c0e072b778db3145aef8055
Daniel Moody <dmoody256@gmail.com>
2017-12-20 23:02:30 -0500

    chekcking envi

--- test/Clang/clang_default_environment.py
+++ test/Clang/clang_default_environment.py
@@ -37,7 +37,7 @@ if not test.where_is('clang'):
 test.write('SConstruct', """
 env = Environment()
 #if env['CC'] != 'clang':
-env['CC'] = 'C:\Program Files\LLVM\bin\clang.exe'
+env['CC'] = '"C:\Program Files/LLVM/bin/clang.exe"'
 env.Program('foo.c')
 """)
 
