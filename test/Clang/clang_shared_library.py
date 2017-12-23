e5c1caa408efc384a0fd06addc8edb9ec0f8c903
Daniel Moody <dmoody256@gmail.com>
2017-12-23 11:08:31 -0500

    update clang

--- test/Clang/clang_shared_library.py
+++ test/Clang/clang_shared_library.py
@@ -26,7 +26,7 @@ __revision__ = "__FILE__ __REVISION__ __DATE__ __DEVELOPER__"
 
 import TestSCons
 from SCons.Environment import Base
-import 
+
 _exe = TestSCons._exe
 test = TestSCons.TestSCons()
 
