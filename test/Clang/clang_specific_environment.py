1fb73fead33d6d5545538f9d5037205107a0232e
Daniel Moody <dmoody256@gmail.com>
2017-12-23 10:44:34 -0500

    checking path

--- test/Clang/clang_specific_environment.py
+++ test/Clang/clang_specific_environment.py
@@ -33,8 +33,6 @@ test = TestSCons.TestSCons()
 if not test.where_is('clang'):
     test.skip_test("Could not find 'clang', skipping test.\n")
 
-if 'win32' in sys.platform:
-    test.skip_test("Not setup for testing windows, skipping test.\n")
 
 test.write('SConstruct', """\
 env = Environment(tools=['clang', 'link'], ENV = os.environ)
