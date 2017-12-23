a5ecb4a95141734d8c210e2a17a436b253701ca1
Daniel Moody <dmoody256@gmail.com>
2017-12-23 10:19:36 -0500

    checking path

--- src/engine/SCons/Action.py
+++ src/engine/SCons/Action.py
@@ -803,6 +803,7 @@ def _subproc(scons_env, cmd, error = 'ignore', **kw):
     kw['env'] = new_env
 
     try:
+        print("running acction " + str(cmd))
         return subprocess.Popen(cmd, shell=True, **kw)
     except EnvironmentError as e:
         if error == 'raise': raise
