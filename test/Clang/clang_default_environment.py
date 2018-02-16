25316a19e8ecb67ee7c1e9fd31078c933feb47bf
Daniel Moody <dmoody256@gmail.com>
2018-02-15 19:48:56 -0500

    the test is forcing clang, trying something else

--- test/Clang/clang_default_environment.py
+++ test/Clang/clang_default_environment.py
@@ -36,8 +36,8 @@ if not test.where_is('clang'):
 
 test.write('SConstruct', """
 env = Environment()
-if env['CC'] != 'clang':
-    env['CC'] = 'clang'
+if env['CC'] != 'clang-cl':
+    env['CC'] = 'clang-cl'
 env.Program('foo.c')
 """)
 
