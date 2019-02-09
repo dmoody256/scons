0603b2301f7856154b77c667cc0e87b5bf9003ea
Daniel <dmoody256@gmail.com>
2019-02-09 02:28:19 -0600

    sdaev

--- test/Repository/SharedLibrary.py
+++ test/Repository/SharedLibrary.py
@@ -51,8 +51,8 @@ f3 = env.SharedObject('f3.c')
 if ARGUMENTS.get('PROGRAM'):
     lib = env.SharedLibrary(target = 'foo',
                             source = f1 + f2 + f3,
-                            WINDOWS_INSERT_DEF = 1,  LINKFLAGS='-install_name @rpath/foo.dylib')
-    env.Program(target='prog', source='prog.c', LIBS='foo', LIBPATH=['.'], RPATH=['%s'])
+                            WINDOWS_INSERT_DEF = 1,  LINKFLAGS='-install_name @executable_path/foo.dylib')
+    env.Program(target='prog', source='prog.c', LIBS='foo', LIBPATH=['.'])
 """ % (test.workpath('work')  ))
 
 for fx in ['1', '2', '3']:
