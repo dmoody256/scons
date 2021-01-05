cb41f2e8edb56b08b4f98f9928e5bab5ab61a050
Daniel Moody <dmoody256@gmail.com>
2021-01-05 13:32:43 -0600

    fixed up missing tests

--- .circleci/check_test_list.sh
+++ .circleci/check_test_list.sh
@@ -1,6 +1,6 @@
 cd ..
 echo "$(find .circleci -name "*.txt" -print -exec cat {} \;)" > current.txt;
-python3 ../runtest.py -l -a > all.txt;
+python3 runtest.py -l -a > all.txt;
 grep -Fxv -f current.txt all.txt
 rm current.txt all.txt
 cd .circleci
